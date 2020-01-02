e7e8fbb9b1b60c480102e9207ec21c11f3cb4b0a
Paw Madsen <pm@nobly.dk>
2020-01-02 13:24:12 +0100

    add more

--- CodeWars/RegexValidatePinCode.cs
+++ CodeWars/RegexValidatePinCode.cs
@@ -4,6 +4,12 @@ namespace CodeWars
 {
     public static class RegexValidatePinCode
     {
+        // My way
         public static bool ValidatePin(string pin) => pin.Any(c => !char.IsDigit(c)) ? false : pin.Length == 4 || pin.Length == 6;
+
+        //public static bool ValidatePin(string pin)
+        //{
+        //    return pin.All(n => Char.IsDigit(n)) && (pin.Length == 4 || pin.Length == 6);
+        //}
     }
 }
