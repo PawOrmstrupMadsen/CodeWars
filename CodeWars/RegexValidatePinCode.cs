3377575aab0b0ed3130f66dd336fd1cd0faa7f38
Paw Madsen <pm@nobly.dk>
2020-01-02 13:22:51 +0100

    Add project files.

---
+++ CodeWars/RegexValidatePinCode.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    public static class RegexValidatePinCode
+    {
+        public static bool ValidatePin(string pin) => pin.Any(c => !char.IsDigit(c)) ? false : pin.Length == 4 || pin.Length == 6;
+    }
+}
---
+++ CodeWars/ReplaceWithAlphabetPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class ReplaceWithAlphabetPosition
+    {
+        public static string AlphabetPosition(string text)
+        {
+            // My way :)
+            // return string.Join(" ", text.Select(c => Char.ToUpper(c) - 64).Where(n => n > 0 && n < 27));
+                        
+            return string.Join(" ", text.ToLower().Where(char.IsLetter).Select(x => x - 'a' + 1));
+        }
+    }
+}
